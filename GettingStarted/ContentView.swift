b6bafc123312f7e42f9620f4369cd6cb53b30257
PranavBP <bellam.pranav@gmail.com>
2024-09-17 14:40:51 -0400

    Initial Commit

---
+++ GettingStarted/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GettingStarted
+//
+//  Created by Pranav Bellam Prasad on 9/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
